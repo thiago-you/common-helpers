a568049b6b1d2cf4e402a3e356ba25cdee56ec22
thiago-you <thiago.youx@gmail.com>
2020-05-14 15:08:23 -0300

    improving word capitalize

--- app/src/main/java/you/thiago/commonhelpers/StringHelper.java
+++ app/src/main/java/you/thiago/commonhelpers/StringHelper.java
@@ -7,27 +7,74 @@ import android.util.Log;
 import androidx.annotation.Nullable;
 import androidx.core.text.HtmlCompat;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Locale;
 
 @SuppressWarnings({"unused", "WeakerAccess"})
 public class StringHelper {
 
+    private static final ArrayList<String> COMMON_NON_CAPITALIZED_WORDS = new ArrayList<String>() {
+        {
+            add("da");
+            add("de");
+            add("do");
+        }
+    };
+
     public static String capitalize(@Nullable String str) {
+        return capitalize(str, null, null);
+    }
+
+    public static String capitalizeWithUpper(@Nullable String str, List<String> withUpper) {
+        return capitalize(str, withUpper, null);
+    }
+
+    public static String capitalizeWithLower(@Nullable String str, List<String> withLower) {
+        return capitalize(str, null, withLower);
+    }
+
+    public static String capitalizeWithUpper(@Nullable String str, String[] withUpper) {
+        return capitalize(str, Arrays.asList(withUpper), null);
+    }
+
+    public static String capitalizeWithLower(@Nullable String str, String[] withLower) {
+        return capitalize(str, null, Arrays.asList(withLower));
+    }
+
+    public static String capitalize(@Nullable String str, List<String> withUpper, List<String> withLower) {
         if (str == null || str.trim().isEmpty() || str.toLowerCase().equals("null")) {
             str = "";
         }
 
-        StringBuilder s = new StringBuilder();
+        if (withUpper == null) {
+            withUpper = new ArrayList<>();
+        }
+        if (withLower == null) {
+            withLower = new ArrayList<>();
+        }
 
-        char ch = ' ';
-        for (int i = 0; i < str.length(); i++) {
-            if (ch == ' ' && str.charAt(i) != ' ') {
-                s.append(Character.toUpperCase(str.charAt(i)));
+        StringBuilder s = new StringBuilder();
+        String[] words = str.split(" ");
+
+        for (String word : words) {
+            if (withUpper.contains(word.toLowerCase())) {
+                word = word.substring(0, 1).toUpperCase() + word.substring(1).toLowerCase();
+            } else  if (withLower.contains(word.toLowerCase())) {
+                word = word.toUpperCase();
+            } else if (word.length() > 2) {
+                word = word.substring(0, 1).toUpperCase() + word.substring(1).toLowerCase();
             } else {
-                s.append(Character.toLowerCase(str.charAt(i)));
+                if (word.length() == 2 && !COMMON_NON_CAPITALIZED_WORDS.contains(word.toLowerCase())) {
+                    word = word.toUpperCase();
+                } else {
+                    word = word.toLowerCase();
+                }
             }
 
-            ch = str.charAt(i);
+            s.append(word);
+            s.append(" ");
         }
 
         return s.toString().trim();
